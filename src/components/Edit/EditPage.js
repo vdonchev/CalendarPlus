201ccab202dcee4cdd260ca9f9d459440e255ad9
Videlin Donchev <ya.5164646777752015@yandex.ru>
2016-12-04 15:49:49 +0200

    Edit form update

--- src/components/Edit/EditPage.js
+++ src/components/Edit/EditPage.js
@@ -1,75 +1,92 @@
 import React, {Component} from 'react';
-import EditForm from './EditForm';
-import {loadTaskDetails, edit} from '../../models/task';
+import EditForm from '../Edit/EditForm';
+import {edit} from '../../models/task';
+import {loadCategories} from '../../models/category';
 
 export default class EditPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {title: '', body: '', submitDisabled: true};
+
+        this.state = {
+            title: this.props.params,
+            body: '',
+            category: '',
+            categories: [<option key='0' value=''>---Choose---</option>],
+            submitDisabled: false
+        };
+
         this.bindEventHandlers();
     }
 
-    componentDidMount() {
-        // Populate form
-        loadTaskDetails(this.props.dateId, this.onLoadSuccess);
-    }
-
     bindEventHandlers() {
-        // Make sure event handlers have the correct context
         this.onChangeHandler = this.onChangeHandler.bind(this);
         this.onSubmitHandler = this.onSubmitHandler.bind(this);
         this.onSubmitResponse = this.onSubmitResponse.bind(this);
         this.onLoadSuccess = this.onLoadSuccess.bind(this);
     }
 
-    onLoadSuccess(response) {
-        this.setState({
-            title: response.title,
-            body: response.body,
-            submitDisabled: false
-        });
-    }
-
     onChangeHandler(event) {
-        event.preventDefault();
-        let newState = {};
-        newState[event.target.name] = event.target.value;
-
-        //TO DO perform validation!!
-
-        this.setState(newState);
-
+        switch (event.target.name) {
+            case 'title':
+                this.setState({title: event.target.value});
+                break;
+            case 'body':
+                this.setState({body: event.target.value});
+                break;
+            case 'category':
+                this.setState({category: event.target.value});
+                break;
+            default:
+                break;
+        }
     }
 
     onSubmitHandler(event) {
         event.preventDefault();
+        this.setState({submitDisabled: true});
 
-        //TO DO perform validation! Task name/description
-
-
-        edit(this.props.params.dateId, this.state.title, this.state.body, this.onSubmitResponse);
+        edit(
+            Number(this.props.params.day),
+            '' + this.props.params.year  + this.props.params.month,
+            this.state.title,
+            this.state.body,
+            this.state.category,
+            this.onSubmitResponse
+        );
     }
 
     onSubmitResponse(response) {
         if (response === true) {
-            // Navigate away from login page
-            this.context.router.push('/');
+            this.context.router.push('/calendar');
         } else {
-            // Something went wrong, let the user try again
-            this.setState({submitDisabled: false});
+            this.setState({submitDisabled: true});
         }
+    }
+
+    onLoadSuccess(categories) {
+        categories.forEach(cat => {
+            this.state.categories.push(<option key={cat._id} value={cat._id}>{cat.title}</option>);
+        });
+
+        this.forceUpdate(); // update number of tasks when ready
+    }
+
+    componentDidMount() {
+        loadCategories(this.onLoadSuccess);
     }
 
     render() {
         return (
             <div>
-                <h1>Edit Task Page</h1>
+                <h1>Edit Task</h1>
                 <EditForm
                     title={this.state.title}
                     body={this.state.body}
+                    category={this.state.category}
                     submitDisabled={this.state.submitDisabled}
                     onChangeHandler={this.onChangeHandler}
                     onSubmitHandler={this.onSubmitHandler}
+                    options={this.state.categories}
                 />
             </div>
         );
